2e90e6c027fe154375aeae6b9ef296c0d661966e
Bhuvesh Kumar <62707309+bhuveshsharma09@users.noreply.github.com>
2021-11-27 21:15:12 +0800

    corrected the Fibonacci function
    corrected the fib function from recursive to non-recursive

--- app.py
+++ app.py
@@ -5,68 +5,94 @@ Created on Fri Nov 26 18:59:39 2021
 @author: Bhuvesh Kumar
 """
 
-from flask import Flask,jsonify,request
-app = Flask(__name__)
+# importing all the required libs
+from flask import Flask, jsonify, request, render_template
+# to avoid Cross-Origin Resource Sharing error
 from flask_cors import CORS, cross_origin
 
+# initialising a flask web server instance
+app = Flask(__name__)
+
+# configuring the CORS object
 cors = CORS(app)
 app.config['CORS_HEADERS'] = 'Content-Type'
 
 
+def fibonacci(n):
+    # the function to compute fibonacci sequence
+    # logic theory: https://www.mathsisfun.com/numbers/fibonacci-sequence.html
+    accumulate = []
 
-def recur_fibo(n):
-   if n <= 1:
-       return n
-   else:
-       return(recur_fibo(n-1) + recur_fibo(n-2))
+    # initialising
+    a, b = 0, 1
+    accumulate.append(a)
+    for i in range(n):
+        accumulate.append(b)
+        # as next number is sum of last two numbers
+        a, b = b, a + b
+    return accumulate
 
 
+def sort_accumulated_numbers(fib_list):
+    # the function to sort Fibonacci numbers as per business logic
+    even_list = []
+    odd_list = []
 
-def sort_fib_list(fib_list):
-    #print(fib_list)
-    even_list=[]
-    odd_list=[]
     for i in range(len(fib_list)):
-        if(fib_list[i]%2==0):
+        # splitting values based on even or odd
+        if fib_list[i] % 2 == 0:
             even_list.append(fib_list[i])
         else:
             odd_list.append((fib_list[i]))
 
+    # using python in-build sort function
     even_list.sort(reverse=True)
     odd_list.sort(reverse=True)
-    #print(even_list)
-    return even_list+odd_list
-
-
-@app.route("/fibonacci", methods=["POST"])
-@cross_origin()
-def starting_url():
-    print(request.json)
-    data = request.json
-    print(data['elements'])
-    
-    num = data['elements']
-    fib_list = []
-    
-    
-    if(num < 1 or num >100 ):
-        return "Wrong value"
-    else:
-        #calculate
-        for i in range(num):
-            fib_list.append(recur_fibo(i))
-
-        sorted_list = sort_fib_list(fib_list)
-        return jsonify({'fibonacci':fib_list,
-                        'sorted':sorted_list})
+    # print(even_list)
+    return even_list + odd_list
 
 
 @app.route('/')
 @cross_origin()
 def Index():
-    return "Welcome to the demo app"
+    # to provide a UI for introduction to the API
+    return "hello"
+    # return render_template("index.html")
 
 
+@app.route("/fibonacci", methods=["POST"])
+@cross_origin()
+def business_logic():
+    # get payload from body of request
+    data = request.json
 
+    # load the value in a local variable
+    num = data['elements']
+
+    # as per the logic
+    # the allowed numbers are from 1 to 100
+    # other all values will no be processed
+    if num < 1 or num > 100:
+        return "Wrong value"
+    else:
+        # get the Fibonacci sequence
+        fibonacci_sequence = fibonacci(num)
+        # preprocess a bit as there is an additional element in the list
+        fibonacci_sequence = fibonacci_sequence[:-1]
+
+        # sort the fibonacci_sequence as per business logic
+        sorted_list = sort_accumulated_numbers(fibonacci_sequence)
+
+        # return the required data in JSON format
+        return jsonify({
+            'fibonacci': fibonacci_sequence,
+            'sorted': sorted_list
+        })
+
+
+# to make sure that this code only runs
+# if the program is being run directly
+# and not being called by other program
 if __name__ == "__main__":
+   # to run the program at flask web server
     app.run(host="0.0.0.0", port=80)
